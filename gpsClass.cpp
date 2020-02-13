7f51daca0741576ef01ab01089d88a9fdfeacbd0
Samuel Peake <peakes@vcu.edu>
2020-02-12 21:19:03 -0500

    saving incase of messing up

--- gpsClass.cpp
+++ gpsClass.cpp
@@ -63,17 +63,47 @@ void gpsClass::find(char x){
   }
   //if the machine has found a money sign and the current char is not a money sign
   // exclude the money sign in the buffer as is unnecessary to print and calc check sum
+  /*if(this->states == found$ && x!='$'&&this->count<5){
+    //cout << "char x in count if: " << x << endl;
+    //cout << "count: " << this->count<<endl;
+      cout << "count: " <<this->count<<endl;
+      cout << "char in: " << x << endl;
+      this->count++;
+      checkType(x);
+      if(this->count==5){
+      //  cout << "Message Type: " << this->msg << endl;
+        this->msg="";
+        this->count=0;
+      }
+    //cout << "char x in count if: " << x << endl;
+  //  cout << "count: " << this->count << endl;
+
+  }
+  /*
+  else if(this->count==5){
+    cout << "enter else"<<endl;
+    //cout << "count: " <<this->count<<endl;
+    if(this->count==5){
+      cout << "Message Type: " << this->msg << endl;
+      this->msg="";
+    }
+  } */
+  bool p=true;
+
   if(this->states == found$ && x!='$'){
     //add characters to the buffer before the asterisk
     if(x!='*'){
       this->buffer+=x;
 
     //cout << "char x out count if: " << x << endl;
+
     if(count<5){
+      //cout << "char x in count if: " << x << endl;
+      //cout << "count: " << this->count<<endl;
       checkType(x);
-      cout << "char x in count if: " << x << endl;
-    //  cout << "count: " << this->count << endl;
+      //cout << "char x in count if: " << x << endl;
     }
+
       /*
       if(count==4){
         cout << "Message Type: " << this->msg << endl;
@@ -82,10 +112,19 @@ void gpsClass::find(char x){
 
       } */
       this->count++;
-    //  cout << "char x out of if: " << x << endl;
-
-      //this is to check if there is an ast and checksum
-
+      if(this->count==5){
+        cout << "this->message: " << this->msg <<endl;
+        this->count=0;
+        p=false;
+        //cout << "count: " <<this->count <<endl;
+        this->msg = "";
+      }
+    //  cout << "count: " <<this->count<<endl;
+    /*
+    if(this->count==0&&p==false){
+      cout << "Message Type: " << this->msg << endl;
+      this->msg = "";
+    } */
       if(x=='\n'){
         this->states = look$;
         //cout << "Message data: "<<this->buffer<<endl;
@@ -93,12 +132,24 @@ void gpsClass::find(char x){
         //call checksum
 
       }
+    //  cout << "count in if: " << this->count << endl;
+    //  cout << "Message data: "<<this->buffer<<endl;
+
     }
     else{//print the message data when the end is reached and reset the buffer
-    //  cout << "Message data: "<<this->buffer<<endl;
+      /*cout << "count " << this->count<< endl;
+      cout << "p" << p<<endl; */
+    /*  if(this->count==0&&p==false){
+        cout << "Message Type: " << this->msg << endl;
+        p=true;
+        this->msg = "";
+      } */
+      //cout << "Message Type: " << this->msg << endl;
+      //cout << "Message data: "<<this->buffer<<endl;
       calcCheckSum();
       this->buffer="";
-      this->count=0;
+      //this->msg = "";
+      //this->count=0;
     }
   }/*
   if(x=='\n'&&this->states==found$){
@@ -147,18 +198,14 @@ void gpsClass::find(char x){
 void gpsClass::checkType(char a){
 //  bool g =false;
 message_type test=none;
-//cout << none << endl;
   if(count>4)
     return;
   if(a=='G'&&this->mtype==none){
     this->mtype = foundG1;
     this->msg += a;
     return;
-  //  cout<<"char: " << a <<endl;
-
-    //g =true;
-    //cout << "1st state: " << this->mtype<<endl;
   }
+  //cout << "msg: " << this->msg<<endl;
   if(this->mtype==none&&a!='G'){
       this->states = look$;
   //    return false;
@@ -176,9 +223,10 @@ message_type test=none;
   //  return false;
       return;
    }
+   /*
    cout << "state after foundG: " << this->mtype<<endl;
    cout << "input char? " << a<<endl;
-   cout << "message1: " <<this->msg<<endl;
+   cout << "message1: " <<this->msg<<endl; */
    if(this->mtype==foundP && a =='G'){
      this->mtype=foundG2;
      this->msg+=a;
@@ -195,27 +243,32 @@ message_type test=none;
     // return false;
         return;
    }
-
+  // cout << "state, 6=R: " << this->mtype << endl;
    if(this->mtype==foundG2 && a=='G'){
      this->mtype=foundG3;
      this->msg+=a;
      return;
    }
    else if(this->mtype==foundG2 && a=='S'){
+     //cout << "S" << endl;
      this->mtype=foundS;
      this->msg+=a;
      return;
    }
    else if(this->mtype==foundR&&a=='M'){
+  //   cout << "R" << endl;
      this->mtype=foundM;
      this->msg+=a;
+     return;
    }
-   else{
+   else if((this->mtype==foundR||this->mtype==foundG2)&&(a!='G'||a!='S'||a!='M')){
      this->states = look$;
      return;
      //return false;
    }
 
+   //cout << "message after foundP: " <<this->msg<<endl;
+   //cout << "char in: " << a << endl;
    if(this->mtype==foundG3 && a=='A'){
      this->mtype=none;
      this->msg+=a;
@@ -223,6 +276,8 @@ message_type test=none;
      return;
      //return true;
    }
+     //cout << "Message Type: " << this->msg << endl;
+   /*
    else if(this->mtype==foundS && a=='V'){
      this->mtype=none;
      this->msg+=a;
@@ -240,6 +295,7 @@ message_type test=none;
    else if(this->mtype==foundM && a == 'C'){
      this->mtype= none;
      this->msg+=a;
+     //cout << "enter c" <<endl;
      this->states=look$;
      return;
      //return true;
@@ -248,7 +304,7 @@ message_type test=none;
      this->states=look$;
      return;
     // return false;
-   }
+  } */
 }
 //void calcCheckSum()
 //Summary:
